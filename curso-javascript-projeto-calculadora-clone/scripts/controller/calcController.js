a1dd3cfa788bbd262e8ce487d793ee1bf28ade47
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-08-15 21:07:21 -0300

    Projeto Calculadora - 14. C09 - Switch

--- curso-javascript-projeto-calculadora-clone/scripts/controller/calcController.js
+++ curso-javascript-projeto-calculadora-clone/scripts/controller/calcController.js
@@ -6,6 +6,7 @@ class CalcController {
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
         this._timeEl = document.querySelector('#hora');
+        this._operation = [];
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
@@ -15,7 +16,7 @@ class CalcController {
 
         this.setDisplayDateTime();
 
-        setInterval(()=> {
+        setInterval(() => {
             this.setDisplayDateTime();
         }, 1000);
     }
@@ -29,14 +30,17 @@ class CalcController {
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
-    initButtonsEvents(){
+    initButtonsEvents() {
         //o sinal de > significa trazer todos os g dentro do id
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
 
             this.addEventListenerAll(btn, 'click drag', e => {
+                let textBtn = btn.className.baseVal.replace("btn-", "");
                 console.log(btn.className.baseVal.replace("btn-", ""));
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
@@ -45,16 +49,89 @@ class CalcController {
         });
     }
 
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+
+                break;
+
+            case 'subtracao':
+
+                break;
+
+            case 'divisao':
+
+                break;
+
+            case 'multiplicacao':
+
+                break;
+
+            case 'porcento':
+
+                break;
+
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+    }
+
     addEventListenerAll(element, events, fn) {
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
         });
     }
 
+    clearAll() {
+        this._operation = [];
+        console.log(this._operation);
+    }
+
+    clearEntry() {
+        this._operation.pop();
+        console.log(this._operation);
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError() {
+        this.displayCalc = "Error..."
+    }
+
     get displayTime() {
         return this._timeEl.innerHTML;
     }
-    
+
     set displayTime(displayTime) {
         this._timeEl.innerHTML = displayTime;
     }
@@ -74,7 +151,7 @@ class CalcController {
     set displayCalc(displayCalc) {
         this._displayCalcEl.innerHTML = displayCalc;
     }
-    
+
     get currentDate() {
         return new Date();
     }
