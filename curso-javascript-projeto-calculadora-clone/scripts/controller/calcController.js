3367434cbf7f3e0d749f4d584cd6fd3e101c1bda
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-09-02 19:41:41 -0300

    Projeto Calculadora - C21 - Tratar outros erros no eval()

--- curso-javascript-projeto-calculadora-clone/scripts/controller/calcController.js
+++ curso-javascript-projeto-calculadora-clone/scripts/controller/calcController.js
@@ -361,7 +361,15 @@ class CalcController {
     }
 
     getResult() {
-        return eval(this._operation.join(""));
+        try {
+            return eval(this._operation.join(""));
+
+        } catch(e) {
+            setTimeout(() => {
+                this.setError();
+            }, 1);
+            console.log(e);
+        }
     }
 
     setError() {
@@ -389,7 +397,7 @@ class CalcController {
     }
 
     set displayCalc(displayCalc) {
-        
+
         if (displayCalc.toString().length > 10) {
             this.setError();
             return false;
