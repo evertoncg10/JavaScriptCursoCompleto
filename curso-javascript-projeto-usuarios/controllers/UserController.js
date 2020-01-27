0290e40499246b0c2ea9bd70a627c3270475a33e
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-01-26 22:14:31 -0300

    Gerenciamento de Usuários - G18 - Validando Formulários

--- curso-javascript-projeto-usuarios/controllers/UserController.js
+++ curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -60,7 +60,15 @@ class UserController {
 
     getValues() {
         let user = {};
+        let isValid = true;
         [...this.formEl.elements].forEach(function(field, index) {
+
+            //Efetua a validação de campos obrigatórios com JS
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add("has-error");
+                isValid = false;
+            }
+
             if(field.name == "gender") {
                 if(field.checked) {
                     user[field.name] = field.value;
@@ -72,6 +80,11 @@ class UserController {
                 user[field.name] = field.value;
             }
         });
+
+        if(!isValid) {
+            return false;
+        }
+
         return new User(
             user.name, 
             user.gender, 
