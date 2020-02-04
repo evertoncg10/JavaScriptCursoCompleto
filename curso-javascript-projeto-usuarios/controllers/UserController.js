a2fa54ebf4fa39067c1ee8afb778e775f87a9030
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-02-04 20:10:43 -0300

    Gerenciamento de Usuários - G26 - Excluindo Dados do Objeto.

--- curso-javascript-projeto-usuarios/controllers/UserController.js
+++ curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -179,7 +179,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -191,6 +191,15 @@ class UserController {
     }
 
     addEventsTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", event => {
+
+            if(confirm("Deseja realmente excluir?")) {
+                tr.remove();
+                this.updateCount();
+            }
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", event => {
             let json = JSON.parse(tr.dataset.user);
 
